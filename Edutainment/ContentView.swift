939a5c6ad5eae902bc5f357d1c59f736cffd01ca
Ramsay Leung <ramsayleung@gmail.com>
2024-01-06 19:55:33 -0800

    Initial Commit

---
+++ Edutainment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Edutainment
+//
+//  Created by ramsayleung on 2024-01-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by ramsayleung on 2024-01-06.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
